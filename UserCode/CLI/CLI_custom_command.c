427f69275e6e89a7e96bc2f3dca9a409b6ae97ca
XY <1023515576@qq.com>
2022-05-25 23:12:16 +0800

    asf

--- UserCode/CLI/CLI_custom_command.c
+++ UserCode/CLI/CLI_custom_command.c
@@ -23,7 +23,7 @@ void vRegisterCustomCLICommands(void)
 	CLI_New_Command(zz, set zhuazi, F_Set_zhuazi, -1);
 	CLI_New_Command(sj, set shengjiang, F_Set_shengjiang, -1);
 	// CLI_New_Command(eposo, set epos_offset, F_Set_epos_offset, -1);
-	CLI_New_Command(kamimadoka, kami.im, F_kamimadoka, 0);
+	// CLI_New_Command(kamimadoka, kami.im, F_kamimadoka, 0);
 	// CLI_New_Command(ads_read_reg, Read all registers of ADS1256, F_ads_read_reg, 0);
 	// CLI_New_Command(ads_read_data_sw, ADS1256 read data switch, F_ads_read_data_sw, 0);
 }
@@ -70,15 +70,15 @@ BaseType_t F_Set_zhuazi(char *pcWriteBuffer, size_t xWriteBufferLen, const char 
 
 	if (pcParameter == NULL) // 说明没有带参数
 	{
-		UD_printf("speed_zhuazi == %.2lg", speed_zhuazi);
-		// UD_printf("CLI_test_var == %.15lg", CLI_test_var); //double 型数据用这行更好
+		// UD_printf("speed_zhuazi == %.2lg", speed_zhuazi);
+		UD_printf("speed_zhuazi == %.15lg", speed_zhuazi); //double 型数据用这行更好
 	}
 	else
 	{
 		speed_zhuazi = atof(pcParameter);
 
-		UD_printf("Set speed_zhuazi = %.2lg", speed_zhuazi);
-		// UD_printf("Set CLI_test_var = %.15lg", CLI_test_var); //double 型数据用这行更好
+		// UD_printf("Set speed_zhuazi = %.2lg", speed_zhuazi);
+		UD_printf("Set speed_zhuazi = %.15lg", speed_zhuazi); //double 型数据用这行更好
 	}
 
 	return pdFALSE; // 结束执行
@@ -93,14 +93,14 @@ BaseType_t F_Set_shengjiang(char *pcWriteBuffer, size_t xWriteBufferLen, const c
 
 	if (pcParameter == NULL) // 说明没有带参数
 	{
-		UD_printf("speed_shengjiang == %.2lg", speed_shengjiang);
-		// UD_printf("CLI_test_var == %.15lg", CLI_test_var); //double 型数据用这行更好
+		// UD_printf("speed_shengjiang == %.2lg", speed_shengjiang);
+		// UD_printf("speed_shengjiang == %.15lg", speed_shengjiang); //double 型数据用这行更好
 	}
 	else
 	{
 		speed_shengjiang = atof(pcParameter);
 
-		UD_printf("Set speed_shengjiang = %.2lg", speed_shengjiang);
+		// UD_printf("Set speed_shengjiang = %.2lg", speed_shengjiang);
 		// UD_printf("Set CLI_test_var = %.15lg", CLI_test_var); //double 型数据用这行更好
 	}
 
