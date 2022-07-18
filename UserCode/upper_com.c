426387e0391c46ca33a6a6ec42b7eed04af659b9
TITH <1023515576@qq.com>
2022-07-19 00:23:50 +0800

    准备上层全位置伺服

--- UserCode/upper_com.c
+++ UserCode/upper_com.c
@@ -24,20 +24,23 @@ void UpperComTaskInit()
 
 void UpperComTask(void const *argument)
 {
-	uint32_t PreviousWakeTime = osKernelSysTick();
+	mavlink_controller_t *ctrl_data = (mavlink_controller_t *) argument;
 	UpperTxData.servo_type = (ServoTypePos << 0) |
-							 (ServoTypeSpeed << 1) |
+							 (ServoTypePos << 1) |
 							 (ServoTypePos << 2);
+
+	uint32_t PreviousWakeTime = osKernelSysTick();
 	for (;;)
 	{
+
 		mavlink_msg_upper_send_struct(MAVLINK_COMM_1, &UpperTxData);
 		osDelayUntil(&PreviousWakeTime, 2);
 	}
 }
 
-void UpperComTaskStart(void *argument)
+void UpperComTaskStart(mavlink_controller_t *ctrl_data)
 {
 	osThreadDef(UpperCom, UpperComTask, osPriorityNormal, 0, 256);
-	osThreadCreate(osThread(UpperCom), argument);
+	osThreadCreate(osThread(UpperCom), ctrl_data);
 }
 
